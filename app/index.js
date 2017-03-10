50c8a5ae83ea6f8bc642f97b5188afe6314f0b4e
Mark Geller <markg@condukia.com>
2017-03-09 23:13:35 -0800

    Set up webpack

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var config = require('./config');
+var config = require('../config');
 
 app.use(express.static(config.staticFolder));
 app.listen(config.port);
+
+console.log("Listening on port " + config.port);
